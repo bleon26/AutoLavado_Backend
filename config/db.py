9e800021df0e191fcc6132a4fa46d32769471c95
Brandon Leon Cabrera <230353@utxicotepec.edu.mx>
2026-02-09 08:57:23 -0600

    archivos completos de la carpeta models

--- config/db.py
+++ config/db.py
@@ -1,11 +1,13 @@
-# db.py
-# Este archivo permite conectar con la base de datos
-
+# pylint: disable=invalid-name
+"""
+Docstring for autolavado_backend.config.db
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://root:1234@127.0.0.1:3308/autolavadoDB"
+
+SQLALCHEMY_DATABASE_URL = "mysql://root:1234@127.0.0.1:3307/autolavadoBD"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
